56a86709c47e24ab933f282027886cec133769b3
Nicolás Bonardi <122838094+nicobonardi@users.noreply.github.com>
2025-02-04 15:57:40 -0300

    Add files via upload

---
+++ TpFinalIntroProg/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <conio2.h>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+// constantes generales
+const int WINDOW = 20;
+const int VEL_INICIAL = 200; // velocidad incial, cambia con el avance
+const int ANCHO = 40;
+const int ALTO = 1200;
+const int VELPROYECTIL = 2000;
+const int TIEMPO_MSJ = 5000;
+const int TIEMPO_INMUNIDAD = 2000;
+const int TIEMPO_PANTALLA_FINAL = 5000;
+int desplazamiento = 0;
+int VELOCIDAD = VEL_INICIAL;
+int puntaje;
+string mensaje;
+bool mensajeActivo;
+int main (int argc, char *argv[]) {
+	
+	return 0;
+}
+
